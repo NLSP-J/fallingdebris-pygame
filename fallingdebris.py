28daf9317adfa282d368aeb5088e540b6e61e7a7
NLSP-J <justin@nullspace.co>
2025-04-22 20:49:27 +0800

    Add files via upload

---
+++ fallingdebris.py
@@ -0,0 +1,133 @@
+import pygame as pg
+import random, time
+pg.init()
+clock = pg.time.Clock()
+
+black = (0, 0, 0)
+
+win_width = 800
+win_height = 600
+screen = pg.display.set_mode((win_width, win_height))
+pg.display.set_caption('Falling Debris')
+
+font = pg.font.Font(None, 30)
+speed = 10
+score = 0
+running = True
+lives = 10
+
+player_size = 40
+player_pos = [win_width / 2, win_height - player_size]  # 400, 600-40
+player_image = pg.image.load('mario.png')
+player_image = pg.transform.scale(player_image, (player_size, player_size))  # 40,40
+
+obj_size = 60
+obj_data = []     # List to store object positions and their images
+obj = pg.image.load('e1.png')
+obj = pg.transform.scale(obj, (obj_size, obj_size))
+
+heart_size = 60
+heart_data = []     
+heart = pg.image.load('heart.png')
+heart = pg.transform.scale(heart, (heart_size, heart_size))
+
+bg_image = pg.image.load('background.png')
+bg_image = pg.transform.scale(bg_image, (win_width, win_height))
+
+
+def create_object(obj_data):
+    if len(obj_data) < 10 and random.random() < 0.1:            
+        x = random.randint(0, win_width - obj_size)
+        y = 0                                         
+        obj_data.append([x, y, obj])
+
+
+def create_heart(heart_data):
+    if len(heart_data) < 2 and random.random() < 0.01:            
+        x = random.randint(0, win_width - heart_size)
+        y = 0                                         
+        heart_data.append([x, y, heart])
+
+
+def update_objects(obj_data):
+    global score
+
+    for object in obj_data:
+        x, y, image_data = object
+        if y < win_height:
+            y += speed
+            object[1] = y
+            screen.blit(image_data, (x, y))
+        else:
+            obj_data.remove(object)
+            score += 1
+
+def update_hearts(heart_data):
+
+    for heart in heart_data:
+        x, y, image_data = heart
+        if y < win_height:
+            y += speed
+            heart[1] = y
+            screen.blit(image_data, (x, y))
+        else:
+            heart_data.remove(heart)
+
+
+def collision_check(obj_data, player_pos):
+    global running
+    for object in obj_data:
+        x, y, image_data = object
+        player_x, player_y = player_pos[0], player_pos[1]
+        obj_rect = pg.Rect(x, y, obj_size, obj_size)
+        player_rect = pg.Rect(player_x, player_y, player_size, player_size)
+        if player_rect.colliderect(obj_rect):
+            time.sleep(2)
+            running = False
+            break
+
+def heart_check(heart_data, player_pos):
+    global score
+    for heart in heart_data:
+        x, y, image_data = heart
+        player_x, player_y = player_pos[0], player_pos[1]
+        heart_rect = pg.Rect(x, y, heart_size, heart_size)
+        player_rect = pg.Rect(player_x, player_y, player_size, player_size)
+        if player_rect.colliderect(heart_rect):
+            score += 10
+            heart_data.remove(heart)
+
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+        if event.type == pg.KEYDOWN:
+            x, y = player_pos[0], player_pos[1]
+            if event.key == pg.K_LEFT:
+                x -= 20
+            elif event.key == pg.K_RIGHT:
+                x += 20
+            player_pos = [x, y]
+
+    screen.blit(bg_image, (0, 0))
+    screen.blit(player_image, (player_pos[0], player_pos[1]))
+
+    text = f'Score: {score}'
+    text = font.render(text, 10, black)
+    screen.blit(text, (win_width - 200, win_height - 40))
+
+    text = f'Lives: {lives}'
+    text = font.render(text, 10, black)
+    screen.blit(text, (win_width - 200, win_height - 20))
+
+    create_object(obj_data)
+    create_heart(heart_data)
+    update_objects(obj_data)
+    update_hearts(heart_data)
+    collision_check(obj_data, player_pos)
+    heart_check(heart_data, player_pos)
+
+    clock.tick(20)
+    pg.display.flip()
