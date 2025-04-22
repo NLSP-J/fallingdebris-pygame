40866493b673e3809b82821680827f6dccbe4ee8
NLSP-J <justin@nullspace.co>
2025-04-22 21:40:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,44 +21,44 @@ player_pos = [win_width / 2, win_height / 2]  # 400, 600-40
 player_image = pg.image.load("./assets/images/mario.png").convert_alpha()
 player_image = pg.transform.scale(player_image, (player_size, player_size))  # 40,40
 
-# obj_size = 60
-# obj_data = []     # List to store object positions and their images
-# obj = pg.image.load("./assets/images/e1.png")
-# obj = pg.transform.scale(obj, (obj_size, obj_size))
+obj_size = 60
+obj_data = []     # List to store object positions and their images
+obj = pg.image.load("./assets/images/e1.png").convert_alpha()
+obj = pg.transform.scale(obj, (obj_size, obj_size))
 
-# bg_image = pg.image.load("./assets/images/background.png")
-# bg_image = pg.transform.scale(bg_image, (win_width, win_height))
+bg_image = pg.image.load("./assets/images/background.png").convert_alpha()
+bg_image = pg.transform.scale(bg_image, (win_width, win_height))
 
 
-# def create_object(obj_data):
-#     if len(obj_data) < 10 and random.random() < 0.1:            
-#         x = random.randint(0, win_width - obj_size)
-#         y = 0                                         
-#         obj_data.append([x, y, obj])
+def create_object(obj_data):
+    if len(obj_data) < 10 and random.random() < 0.1:            
+        x = random.randint(0, win_width - obj_size)
+        y = 0                                         
+        obj_data.append([x, y, obj])
 
-# def update_objects(obj_data):
-#     global score
+def update_objects(obj_data):
+    global score
 
-#     for object in obj_data:
-#         x, y, image_data = object
-#         if y < win_height:
-#             y += speed
-#             object[1] = y
-#             screen.blit(image_data, (x, y))
-#         else:
-#             obj_data.remove(object)
-#             score += 1
+    for object in obj_data:
+        x, y, image_data = object
+        if y < win_height:
+            y += speed
+            object[1] = y
+            screen.blit(image_data, (x, y))
+        else:
+            obj_data.remove(object)
+            score += 1
 
-# def collision_check(obj_data, player_pos):
-#     global running
-#     for object in obj_data:
-#         x, y, image_data = object
-#         player_x, player_y = player_pos[0], player_pos[1]
-#         obj_rect = pg.Rect(x, y, obj_size, obj_size)
-#         player_rect = pg.Rect(player_x, player_y, player_size, player_size)
-#         if player_rect.colliderect(obj_rect):
-#             while True:
-#                 pass
+def collision_check(obj_data, player_pos):
+    global running
+    for object in obj_data:
+        x, y, image_data = object
+        player_x, player_y = player_pos[0], player_pos[1]
+        obj_rect = pg.Rect(x, y, obj_size, obj_size)
+        player_rect = pg.Rect(player_x, player_y, player_size, player_size)
+        if player_rect.colliderect(obj_rect):
+            pg.quit()
+            sys.exit()
 
 async def main():
     global player_pos
@@ -72,16 +72,16 @@ async def main():
                     x += 20
                 player_pos = [x, y]
     
-        # screen.blit(bg_image, (0, 0))
+        screen.blit(bg_image, (0, 0))
         screen.blit(player_image, (player_pos[0], player_pos[1]))
     
-        # text = f'Score: {score}'
-        # text = font.render(text, 10, black)
-        # screen.blit(text, (win_width - 200, win_height - 40))
+        text = f'Score: {score}'
+        text = font.render(text, 10, black)
+        screen.blit(text, (win_width - 200, win_height - 40))
     
-        # create_object(obj_data)
-        # update_objects(obj_data)
-        # collision_check(obj_data, player_pos)
+        create_object(obj_data)
+        update_objects(obj_data)
+        collision_check(obj_data, player_pos)
     
         pg.display.update()
         clock.tick(30)
